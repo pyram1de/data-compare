33167e87d589f098f9176e1defd4cc5d61a58641
David Hulmes <hulmesd@usshq.co.uk>
2017-03-22 10:22:13 +0000

    updated to include generators

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 var MongoClient = require('mongodb').MongoClient;
 var fs = require('fs');
-
 var url = 'mongodb://localhost:27017/joinc';
 var targetDir = 'E:\\Temp\\fullExtract';
 var newLine = '\r\n';
+var tab = '\t';
 var hasHeader = true;
 var outputFile = 'E:\\Temp\\joinersNoExist';
 var outputFileSuccess = 'E:\\Temp\\joinersExist';
 var outputAll = "E:\\Temp\\outputAll";
 var outputOne = "E:\\Temp\\outputOne";
+var outputLog = "E:\\Temp\\FilteredExtract\\output.log";
+var outputDir = "E:\\Temp\\FilteredExtract\\";
 var foundCount = 0;
-var processFile = function(file,db){   
+var headerDone, process;
+
+if(fs.existsSync(outputLog)){ // remove existing file
+    fs.unlinkSync(outputLog);
+};
+
+run(function* myGenerator(resume){
+    var db = yield MongoClient.connect(url, resume);
+    var files = fs.readdirSync(targetDir);
+    for(var i = 0;i<files.length;i++){
+    var file = files[i];
+    console.log(file);
     var potentialNumber = file.substr(2,4);
     var inst = parseInt(potentialNumber, 10);
     var instInFile = false;
@@ -18,83 +31,137 @@ var processFile = function(file,db){
         instInFile = true;
     }
     var filename = targetDir + '\\' + file;
-    console.log(filename);
     var fileContents = fs.readFileSync(filename, 'utf8');
     var startRow = 0;
     if(hasHeader){
         startRow = 1;
     }
     var fileRows = fileContents.split(newLine);
-    //console.log(fileRows[startRow]);
-    var initial = 1;
-    fileRows.forEach(function(row){
+    var initial = 1;      
+    headerDone = false;
+    for(var x = 0;x<fileRows.length;x++){
+        process = true;
+        var row = fileRows[x];
         if(initial&&hasHeader)
         {
             initial = 0;
-            return;
+            continue;
         }
         var rowData = row.split('\t');
         var employer = instInFile ? 'FS' : 'CRBS';
         var nino = rowData[6];
         var dob = rowData[7];
         var surname = rowData[4];
+        var firstName = rowData[5];
         var doj = rowData[11];
         var institution = instInFile ? rowData[rowData.length-1] : inst;
         if(nino!==undefined){
-            //console.log(type + ' ' + nino +  ' ' + dob  +  ' ' + doj  +  ' ' +  institution);        
             // find joiners....
             var collection = db.collection('hartlinkmembers');
-            collection.findOne({
-                "Employer": institution,
+            var ensdd = yield collection.findOne({
+                $or: [{"Employer": institution},
+                      {"Employer": "MULTIPLE"}],
                 "Nino": nino.substr(0,8),
-                //"Surname": surname,
+                "Surname": surname.trim(),
                 "DOB" : dob,
                 "DJS" : doj
-            },function(err, result){
-                if(err){
-                    console.log(err);
-                    return;
+            },resume);
+            var n = yield collection.findOne({
+                "Nino": nino.substr(0,8)
+            },resume);
+            var en = yield collection.findOne({
+                $or: [{"Employer": institution},
+                      {"Employer": "MULTIPLE"}],
+                "Nino": nino.substr(0,8)
+            },resume);
+            var esd = yield collection.findOne({
+                $or: [{"Employer": institution},
+                      {"Employer": "MULTIPLE"}],
+                "Surname": surname.trim(),
+                "DOB" : dob
+            },resume);
+            var esdd = yield collection.findOne({
+                $or: [{"Employer": institution},
+                      {"Employer": "MULTIPLE"}],
+                "Surname": surname.trim(),
+                "DOB" : dob.trim(),
+                "DJS" : doj.trim()
+            },resume);
+            var sd = yield collection.findOne({
+                "Surname": surname.trim(),
+                "DOB" : dob
+            },resume);
+            var fsd = yield collection.findOne({
+                "Surname": surname.trim(),
+                "First name": firstName.trim(),
+                "DOB" : dob
+            },resume); 
+            var fsds = yield collection.findOne({
+                "Surname": firstName.trim(),
+                "First name": surname.trim(),
+                "DOB" : dob
+            },resume);
+
+            switch(nino){ // granular exclusion
+                case "JP256044A":
+                console.log('excluding member: ' +nino);
+                process = false;
+                break;
+            }
+
+            if(n!==null){ // nino not found.
+                process=false;
+            }
+
+            if(esd!==null){ // employer surname dob not found
+                process=false;
+            }
+
+            if(fsds!==null){
+                process=false;
+            }
+
+            if(process===true){
+                // output file.
+                if((!headerDone)&&hasHeader){
+                    if(fs.existsSync(outputDir+file)){ // remove existing file
+                        fs.unlinkSync(outputDir+file);
+                    };
+                    fs.appendFileSync(outputDir+file, fileRows[0]+ newLine);
+                    headerDone=true;
                 }
-                if(employer==='FS'){
-                        //fs.appendFileSync(outputAll + 'FS.log', rowData.join('\t') + newLine,'utf8');                                           
-                    } else {
-                        //fs.appendFileSync(outputAll + 'CRBS.log', rowData.join('\t') + newLine,'utf8');                                           
-                }                                          
-                if(result===null){
-                    // append to log...
-                    //console.log('employer ' + institution + ', nino ' + nino.substr(0,8) +  ', surname ' + surname  +  ', dob ' + dob  +  ', doj ' +  doj);   
-                    fs.appendFileSync(outputOne + '.log', institution +'\t' + employer+'\tnot found\t'+rowData.join('\t') + newLine,'utf8');                                           
-                    if(employer==='FS'){
-                        //fs.appendFileSync(outputFile + 'FS.log', rowData.join('\t') + newLine,'utf8');                                           
-                    } else {
-                        //fs.appendFileSync(outputFile + 'CRBS.log', rowData.join('\t') + newLine,'utf8');                                           
-                    }                   
-                } else {
-                    // found.....    
-                    fs.appendFileSync(outputOne + '.log', institution +'\t' + employer+'\tfound\t'+rowData.join('\t') + newLine,'utf8');                                           
-                    foundCount += 1;
-                    if(employer==='FS'){
-                        //fs.appendFileSync(outputFileSuccess + 'FS.log', rowData.join('\t') + newLine,'utf8');                                           
-                    } else {
-                        //fs.appendFileSync(outputFileSuccess + 'CRBS.log', rowData.join('\t') + newLine,'utf8');                                           
-                    }                    
-                }                             
-            })        
+                fs.appendFileSync(outputDir+file, rowData.join(tab) + newLine, 'utf8');
+                fs.appendFileSync(outputLog, rowData.join(tab) + newLine,'utf8');
+            }
 
-        }       
-
-    });
-    //db.close();
-}
-
-MongoClient.connect(url, function(err, db){
-    console.log('connected to mongo server');
-    
-    var files = fs.readdirSync(targetDir);
-    files.forEach(function(file){ // foreach is syncronous        
-        processFile(file,db);        
-    });    
-    //console.log('found count...');
-    //console.log(foundCount);
-    //db.close();
+            fs.appendFileSync(outputOne + '.tdf', 
+                    institution+tab + 
+                    employer+tab+
+                    (en?'matched':'')+tab+
+                    (n?'matched':'')+tab+
+                    (ensdd?'matched':'')+tab+
+                    (esd?'matched':'')+tab+
+                    (esdd?'matched':'')+tab+
+                    (sd?'matched ' + sd["First name"]:'')+tab+
+                    (fsd?'matched':'')+tab+
+                    (fsds?'matched':'')+tab+
+                    rowData.join(tab) + newLine,'utf8');
+        }
+    }
+    };
+    db.close();
+    console.log('done');
 });
+
+function run(generatorFunction){
+    var generatorItr = generatorFunction(resume);
+    function resume(err, callbackValue){
+        if(err){
+            console.log(err);
+            generatorItr.next(null);
+        } else {
+            generatorItr.next(callbackValue);
+        }
+    }
+    generatorItr.next();
+};
